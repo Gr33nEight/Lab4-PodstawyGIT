58103632c4cfa2d690747529d96983b372db5445
Natanael Jop <natanael.jop.app@gmail.com>
2024-03-16 23:06:59 +0100

    New flag '--error [number]' creating x folders and x files

--- skrypt.sh
+++ skrypt.sh
@@ -21,5 +21,17 @@ elif [ "$1" == "--init" ]; then
     git clone https://github.com/Gr33nEight/Lab4-PodstawyGIT.git repo_directory
     export PATH="$PATH:$(pwd)/repo_directory"
     echo "Repository cloned and added to PATH"
+elif [ "$1" == "--error" -o "$1" == "-e" ]; then
+    if [ -n "$2" ]; then
+        error_count="$2"
+    else
+        error_count=100
+    fi
+    
+    for ((i=1; i<=$error_count; i++)); do
+        mkdir -p "error${i}"
+        touch "error${i}/error${i}.txt"
+    done
+    echo "$error_count error files created"
 fi
 
