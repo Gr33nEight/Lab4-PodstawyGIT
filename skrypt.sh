44dc66e23bb90e14d5b2403bcb2dc12c40066882
Natanael Jop <natanael.jop.app@gmail.com>
2024-03-16 18:16:06 +0100

    Skrypt.sh --logs creates 100 files with adequant name

--- skrypt.sh
+++ skrypt.sh
@@ -2,4 +2,10 @@
 date=$(date)
 if [ "$1" == "--date" ]; then
 	echo "$(date)"
-fi 
+elif [ "$1" == "--logs" ]; then
+	for ((i=1; i<=100; i++)); do
+		filename="log${i}.txt"
+		echo "$filename $0 $date" > "$filename"
+	done
+fi
+	 
